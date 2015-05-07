783e7e0f36246e5686fe9a02950113e040c6e3bd
Charles Lavery <charles.lavery@gmail.com>
2015-05-07 19:57:28 -0400

    remove bower

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
-var _ = require('underscore');
 var openpgp = require('../bower_components/openpgp/dist/openpgp.js');
 var PUBLIC_KEY = require("raw!../charleslavery.txt");
 var publicKey = openpgp.key.readArmored(PUBLIC_KEY);
@@ -31,6 +30,7 @@ var App = React.createClass({
       <div className="container" style={style.container}>
         <div className="row">
           <h1>Securely Message Charles Lavery</h1>
+          <p><a href="mailto:charles.lavery@gmail.com">charles.lavery@gmail.com</a></p>
           <label>Enter Message Here:</label><br/>
           <textarea style={style.container} className="form-control" rows="20" ref="source"></textarea><br/>
           <button className="btn btn-primary" onClick={this.didClickEncrypt}>Click To Encrypt</button>
